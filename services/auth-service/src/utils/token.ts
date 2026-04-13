83c6b171d6f0bf2637f7648b1ccb4bf5dcf7a700
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2026-04-13 17:49:29 +0200

    feat(auth-service): implement JWT authentication with bcrypt and add user registration functionality

---
+++ services/auth-service/src/utils/token.ts
@@ -0,0 +1,43 @@
+import { env } from '@/config/env';
+import bcrypt from 'bcrypt';
+import jwt, { type Secret, type SignOptions } from 'jsonwebtoken';
+
+const ACCEESS_TOKEN: Secret = env.JWT_SECRET;
+const REFRESH_TOKEN: Secret = env.JWT_REFRESH_SECRET;
+const ACCESS_OPTIONS: SignOptions = { expiresIn: env.JWT_EXPIRES_IN as SignOptions['expiresIn'] };
+const REFRESH_OPTIONS: SignOptions = {
+  expiresIn: env.JWT_REFRESH_EXPIRES_IN as SignOptions['expiresIn'],
+};
+
+// Helper function to hash a password using bcrypt
+export const hashPassword = async (password: string): Promise<string> => {
+  const saltRounds = 12;
+  return await bcrypt.hash(password, saltRounds);
+};
+
+// Helper function compare a plaintext password with a hashed password
+export const verifyPassword = async (password: string, hash: string): Promise<boolean> => {
+  return await bcrypt.compare(password, hash);
+};
+
+export interface AccessTokenPayload {
+  sub: string; // user ID
+  email: string;
+}
+
+export interface RefreshTokenPayload {
+  sub: string; // user ID
+  tokenId: string;
+}
+
+export const signAccessToken = (payload: AccessTokenPayload): string => {
+  return jwt.sign(payload, ACCEESS_TOKEN, ACCESS_OPTIONS);
+};
+
+export const signRefreshToken = (payload: RefreshTokenPayload): string => {
+  return jwt.sign(payload, REFRESH_TOKEN, REFRESH_OPTIONS);
+};
+
+export const verifyRefreshToken = (payload: string): RefreshTokenPayload => {
+  return jwt.verify(payload, REFRESH_TOKEN) as RefreshTokenPayload;
+};
